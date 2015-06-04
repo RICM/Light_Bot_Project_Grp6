bd949562a830317a749e3191f71faaec7fe4fb2e
Lucidarme <benjamin.lucidarme@gmail.com>
2015-06-04 16:34:30 +0200

    Case by Ben

--- src/map/Painted_Case.java
+++ src/map/Painted_Case.java
@@ -1,5 +1,13 @@
 package map;
+import Couleur.*;
 
 public class Painted_Case extends abstr_Case {
 
+	
+		public Painted_Case(int h, Couleur color, Coordonnees cordo){
+			this.couleur = color;
+			this.coordonnees = cordo;
+			this.hauteur = h;
+		}
+		
 }
