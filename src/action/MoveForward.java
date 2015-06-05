77959a597a074857db75d6dbdeb22be4a9f574a7
chi-miel <sipasseuth.adrien@gmail.com>
2015-06-05 11:18:04 +0200

    correction MV et fais JUMP

--- src/action/MoveForward.java
+++ src/action/MoveForward.java
@@ -8,8 +8,9 @@ import exception.MouvementEx;
 public class MoveForward implements int_Action{
 	
 	public static void execute(Robot r) throws MouvementEx{
-		abstr_Case c_prime;
+		abstr_Case c_prime = null;
 		Coordonnees pos = r.getCurrent_Case().get_coordonnees();
+		
 		switch (r.getOrientation()) {  
 	    	case  TOP :
 	    		c_prime = World.currentWorld.get_terrain(pos.get_n()).get_case(pos.get_x(),pos.get_y()-1);
@@ -19,11 +20,13 @@ public class MoveForward implements int_Action{
 	    		c_prime = World.currentWorld.get_terrain(pos.get_n()).get_case(pos.get_x()-1,pos.get_y());
 	    	case  RIGHT :
 	    		c_prime = World.currentWorld.get_terrain(pos.get_n()).get_case(pos.get_x()+1,pos.get_y());
+		}
+		
 		if (! isPossible(r,c_prime)){
 			throw (new MouvementEx("impossible d'avancer"));
 		} else {
 			r.setCurrent_Case(c_prime);
-		}}
+		}
 		
 	}
 	
