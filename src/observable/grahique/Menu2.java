84c62590e946bf2c80f05b78e967c177d47447bb
ghamm <misa7403@hotmail.fr>
2015-06-09 10:54:09 +0200

    interface

--- src/observable/grahique/Menu2.java
+++ src/observable/grahique/Menu2.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -19,9 +21,7 @@ public class Menu2 extends JFrame implements ActionListener{
 
 	//Panel
 	private final JPanel contentPane;
-	private JPanel panNorth;
 	private JPanel panCenter;
-	private JPanel panSouth;
 
 	private JButton btnFullScreen;
 	private JButton btnJouer;
@@ -29,7 +29,7 @@ public class Menu2 extends JFrame implements ActionListener{
 	private JLabel imageMenu;
 
 	public int ImageLargeur = 600;
-	public int ImageHauteur = 280;
+	public int ImageHauteur = 200;
 	/**
 	 * Create the frame.
 	 */
@@ -46,20 +46,44 @@ public class Menu2 extends JFrame implements ActionListener{
 
 		setPanels();
 		setComponents();
-
-		this.contentPane.add(this.panNorth,BorderLayout.NORTH);
+		setBackgr();
+		
 		this.contentPane.add(this.panCenter, BorderLayout.CENTER);
-		this.contentPane.add(panSouth,BorderLayout.SOUTH);
+
 
 
 		this.setContentPane(this.contentPane);
 	}
 
+	public void setBackgr() {
+		ImageIcon icon = new ImageIcon("menu/back1.jpg");
+		Image img = icon.getImage() ;
+		Image newimg = img.getScaledInstance( this.getWidth(), this.getHeight(),  java.awt.Image.SCALE_SMOOTH ) ;
+		icon = new ImageIcon( newimg );
+		JLabel backMenu1 = new JLabel(icon);
+		backMenu1.setBounds(0, 0, this.getWidth(), this.getHeight());
+		
+		URL url;
+		try {
+			url = new URL("https://darylmcmahon.files.wordpress.com/2014/05/snow_falling2.gif");
+			ImageIcon imageIcon = new ImageIcon(url);
+			JLabel label = new JLabel(imageIcon);
+			label.setBounds(0, 0, getWidth(), getHeight());
+			panCenter.add(label);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		panCenter.add(backMenu1);
+		
+	}
+
 	public void setPanels(){
-		this.panNorth = new JPanel();
-		this.panNorth.setPreferredSize(new Dimension(this.getSize().width, this.getSize().height/6));
-		this.panNorth.setBackground(new Color(255, 255, 255));
-		this.panNorth.setLayout(null);
+//		this.panNorth = new JPanel();
+//		this.panNorth.setPreferredSize(new Dimension(this.getSize().width, this.getSize().height/6));
+//		this.panNorth.setBackground(new Color(255, 255, 255));
+//		this.panNorth.setLayout(null);
 
 
 		this.panCenter = new JPanel();
@@ -67,10 +91,10 @@ public class Menu2 extends JFrame implements ActionListener{
 		this.panCenter.setBackground(new Color(255, 255, 255));
 		this.panCenter.setLayout(null);
 
-		this.panSouth = new JPanel();
-		this.panSouth.setPreferredSize(new Dimension(this.getSize().width, this.getSize().height/6));
-		this.panSouth.setBackground(new Color(255, 255, 255));
-		this.panSouth.setLayout(null);
+//		this.panSouth = new JPanel();
+//		this.panSouth.setPreferredSize(new Dimension(this.getSize().width, this.getSize().height/6));
+//		this.panSouth.setBackground(new Color(255, 255, 255));
+//		this.panSouth.setLayout(null);
 	}
 
 	public void setComponents(){
@@ -79,20 +103,29 @@ public class Menu2 extends JFrame implements ActionListener{
 		this.btnFullScreen.setBounds(this.getSize().width/11, getSize().height/12-20, 150, 40);
 		this.btnFullScreen.addActionListener(this);
 
-		ImageIcon icon = new ImageIcon("light_bot.jpg");
-		Image img = icon.getImage() ;
-		Image newimg = img.getScaledInstance( ImageLargeur, ImageHauteur,  java.awt.Image.SCALE_SMOOTH ) ;
-		icon = new ImageIcon( newimg );
-		this.imageMenu = new JLabel(icon);
-		this.imageMenu.setBounds(this.getSize().width/2-315, 0, ImageLargeur, ImageHauteur);
+//		ImageIcon icon = new ImageIcon("light_bot.jpg");
+//		Image img = icon.getImage() ;
+//		Image newimg = img.getScaledInstance( ImageLargeur, ImageHauteur,  java.awt.Image.SCALE_SMOOTH ) ;
+//		icon = new ImageIcon( newimg );
+//		this.imageMenu = new JLabel(icon);
+//		this.imageMenu.setBounds(this.getSize().width/2-315, 0, ImageLargeur, ImageHauteur);
 
-		this.btnJouer = new JButton("Jouer");
+		ImageIcon btnPlay = new ImageIcon("menu/play.png");
+//		Image img = btnPlay.getImage() ;
+//		Image newimg = img.getScaledInstance( 200, 200,  java.awt.Image.SCALE_SMOOTH ) ;
+//		btnPlay = new ImageIcon( newimg );
+		this.btnJouer = new JButton(btnPlay);
 		this.btnJouer.setBackground(new Color(255, 255, 255));
-		this.btnJouer.setBounds(this.getSize().width/2-90, ImageHauteur + 50, 180, 44);
+		btnJouer.setOpaque(false);
+		btnJouer.setBorder(null);
+		btnJouer.setBorderPainted(false);
+		btnJouer.setContentAreaFilled(false);
+
+		this.btnJouer.setBounds(this.getSize().width/2-90, ImageHauteur, 200, 200);
 		this.btnJouer.addActionListener(this);
 
-		this.panNorth.add(this.btnFullScreen);
-		this.panCenter.add(this.imageMenu);
+//		this.panNorth.add(this.btnFullScreen);
+//		this.panCenter.add(this.imageMenu);
 		this.panCenter.add(this.btnJouer);
 	}
 
--- src/observable/grahique/SelectLevel.java
+++ src/observable/grahique/SelectLevel.java
@@ -4,9 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -17,12 +19,10 @@ import javax.swing.border.EmptyBorder;
 public class SelectLevel extends JDialog implements ActionListener{
 
 	private final JPanel contentPanel;
-	private final JPanel panNorth;
 	private final JPanel panCenter;
 	private final JLabel labLevel;
 	private final JButton [] tabJbutton = new JButton [3];
-	private final JButton btnRetour;
-	private final JPanel panSouth;
+//	private final JButton btnRetour;
 	private final Menu2 frame;
 
 
@@ -35,22 +35,22 @@ public class SelectLevel extends JDialog implements ActionListener{
 		this.setBounds(100, 100, 1200, 700);
 		this.getContentPane().setLayout(new BorderLayout());
 		this.contentPanel.setLayout(new FlowLayout());
-		this.contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.contentPanel.setBorder(new EmptyBorder(0, 0, 0, 0));
 		this.setResizable(false);
 		this.setModal(true);
 
-		this.panNorth = new JPanel();
-		this.panNorth.setPreferredSize(new Dimension(1200, 75));
-		this.panNorth.setBackground(new Color(255, 255, 255));
-		this.panNorth.setLayout(null);
-
-		this.panSouth = new JPanel();
-		this.panSouth.setPreferredSize(new Dimension(1200, 75));
-		this.panSouth.setBackground(new Color(255, 255, 255));
-		this.panSouth.setLayout(null);
+//		this.panNorth = new JPanel();
+//		this.panNorth.setPreferredSize(new Dimension(1200, 75));
+//		this.panNorth.setBackground(new Color(255, 255, 255));
+//		this.panNorth.setLayout(null);
+//
+//		this.panSouth = new JPanel();
+//		this.panSouth.setPreferredSize(new Dimension(1200, 75));
+//		this.panSouth.setBackground(new Color(255, 255, 255));
+//		this.panSouth.setLayout(null);
 
 		this.panCenter = new JPanel();
-		this.panCenter.setPreferredSize(new Dimension(800, 500));
+		this.panCenter.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()));
 		this.panCenter.setBackground(new Color(255, 255, 255));
 		this.panCenter.setLayout(null);
 
@@ -58,7 +58,7 @@ public class SelectLevel extends JDialog implements ActionListener{
 		labLevel.setFont(new Font("Serif", Font.PLAIN, 25));
 		this.labLevel.setBounds(this.getSize().width/2,0,182,34);
 
-		this.panNorth.add(this.labLevel);
+//		this.panNorth.add(this.labLevel);
 
 		int x = 30;
 		int y = 30;
@@ -80,16 +80,18 @@ public class SelectLevel extends JDialog implements ActionListener{
 				x=30;
 			}
 		}
+		
+		setBackgr();
 
-		this.btnRetour = new JButton("Menu");
-		this.btnRetour.setBackground(new Color(255, 255, 255));
-		this.btnRetour.setBounds(15, 16, 109, 29);
-		this.btnRetour.addActionListener(this);
-		this.panSouth.add(this.btnRetour);
+//		this.btnRetour = new JButton("Menu");
+//		this.btnRetour.setBackground(new Color(255, 255, 255));
+//		this.btnRetour.setBounds(15, 16, 109, 29);
+//		this.btnRetour.addActionListener(this);
+//		this.panSouth.add(this.btnRetour);
 
-		this.contentPanel.add(this.panNorth,BorderLayout.NORTH);
+//		this.contentPanel.add(this.panNorth,BorderLayout.NORTH);
 		this.contentPanel.add(this.panCenter, BorderLayout.CENTER);
-		this.contentPanel.add(this.panSouth,BorderLayout.SOUTH);
+//		this.contentPanel.add(this.panSouth,BorderLayout.SOUTH);
 
 
 		this.setContentPane(this.contentPanel);
@@ -97,12 +99,25 @@ public class SelectLevel extends JDialog implements ActionListener{
 	}
 
 
+	public void setBackgr() {
+		ImageIcon icon = new ImageIcon("selectLvl/back.jpg");
+		Image img = icon.getImage() ;
+		Image newimg = img.getScaledInstance( this.getWidth(), this.getHeight()+100,  java.awt.Image.SCALE_SMOOTH ) ;
+		icon = new ImageIcon( newimg );
+		JLabel backMenu1 = new JLabel(icon);
+		backMenu1.setBounds(0, -10, this.getWidth(), this.getHeight());
+		
+		panCenter.add(backMenu1);
+		
+	}
+
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource()==this.btnRetour){
-			this.dispose();
-		}
+//		if(e.getSource()==this.btnRetour){
+//			this.dispose();
+//		}
 
 		if(e.getSource()==tabJbutton[0]){
 			this.dispose();
