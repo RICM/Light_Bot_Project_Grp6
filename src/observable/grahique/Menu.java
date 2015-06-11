d0eb54b627238113560973a91d44afdc262df2d9
ghamm <misa7403@hotmail.fr>
2015-06-11 16:32:17 +0200

    FULLSCREEN

--- src/observable/grahique/Menu.java
+++ src/observable/grahique/Menu.java
@@ -5,6 +5,9 @@ import java.nio.file.Paths;
 
 import javax.swing.JOptionPane;
 
+import observable.action.MoveForward;
+import observable.robot.Orientation;
+
 import org.jsfml.graphics.ConstView;
 import org.jsfml.graphics.FloatRect;
 import org.jsfml.graphics.RenderWindow;
@@ -14,11 +17,16 @@ import org.jsfml.graphics.View;
 import org.jsfml.system.Vector2f;
 import org.jsfml.system.Vector2i;
 import org.jsfml.window.Mouse;
+import org.jsfml.window.Keyboard.Key;
 import org.jsfml.window.Mouse.Button;
+import org.jsfml.window.Keyboard;
 import org.jsfml.window.VideoMode;
 import org.jsfml.window.WindowStyle;
 import org.jsfml.window.event.Event;
 import org.jsfml.window.event.Event.Type;
+
+import exception.MouvementEx;
+import exception.UnreachableCase;
 
 public class Menu {
 
@@ -27,12 +35,14 @@ public class Menu {
 	protected Sprite sprite_Background = new Sprite();
 	protected Texture texture_Play= new Texture();
 	protected Sprite sprite_Play = new Sprite();
-//	protected View camera = new View();
+	protected View camera = new View();
 	protected static final int WIDTH = 1200;
 	protected static final int HEIGHT = 700;
 
 	public Menu(){
-//		Menu.app.setView(camera);
+		camera.setSize(new Vector2f(WIDTH, HEIGHT));
+		camera.setCenter(WIDTH/2, HEIGHT/2);
+		Menu.app.setView(camera);
 		while(Menu.app.isOpen()){
 			this.processEvent();
 			this.displayBackground();
@@ -47,7 +57,7 @@ public class Menu {
 			this.texture_Play.loadFromFile(Paths.get("menu/play.png"));
 			sprite_Play = new Sprite();
 			this.sprite_Play.setTexture(this.texture_Play);
-			this.sprite_Play.setPosition(Menu.WIDTH/2-75, Menu.HEIGHT/2-75);
+			this.sprite_Play.setPosition(Menu.app.getSize().x/2-75, Menu.app.getSize().y/2-75);
 			Menu.app.draw(this.sprite_Play);
 
 		} catch (IOException e) {
@@ -78,7 +88,31 @@ public class Menu {
 				Menu.app.close();
 			}
 			if(e.type == Event.Type.RESIZED){
-				
+				int x = Menu.app.getSize().x;
+				int y = Menu.app.getSize().y;
+				camera.setSize(x, y);
+				camera.setCenter(x/2, y/2);
+				Menu.app.setView(camera);
+			}
+			
+			if (Keyboard.isKeyPressed(Key.DOWN)){
+				camera.zoom(0.5f);
+				Menu.app.setView(camera);
+			}
+			
+			if (Keyboard.isKeyPressed(Key.UP)){
+				camera.zoom(2f);
+				Menu.app.setView(camera);
+			}
+			
+			if (Keyboard.isKeyPressed(Key.RIGHT)){
+				camera.rotate(-45);;
+				Menu.app.setView(camera);
+			}
+			
+			if (Keyboard.isKeyPressed(Key.LEFT)){
+				camera.rotate(45);;
+				Menu.app.setView(camera);
 			}
 
 			if (e.type == Event.Type.MOUSE_BUTTON_PRESSED && Mouse.isButtonPressed(Button.LEFT)) {
