6be0c52da1a747e0868c2e7b5c3ff80cdc65cc9a
Damien <damien.crastes@gmail.com>
2015-06-12 16:26:03 +0200

    correctif doublon dans une liste d'action possible

--- src/observable/action_list/Possible_List.java
+++ src/observable/action_list/Possible_List.java
@@ -5,37 +5,44 @@ import java.util.LinkedList;
 
 import observable.int_Observable;
 import observable.action.int_Action;
-import observable.robot.Robot;
 import observer.int_Observer;
-import exception.ActionEx;
-import exception.UnreachableCase;
-import exception.MouvementEx;
 
 public class Possible_List implements int_Action_List, int_Observable{
-	
+
 	private LinkedList<int_Action> allowed_actions = new LinkedList<int_Action>();
-	
-	private ArrayList<int_Observer> listObserver = new ArrayList<int_Observer>(); 
-	
+
+	private ArrayList<int_Observer> listObserver = new ArrayList<int_Observer>();
+
+	@Override
 	public void addActionToList(int_Action act){
-		//System.out.println("trying to add : "+act.getClass().getCanonicalName());
-		allowed_actions.add(act);
-		if (allowed_actions.contains(act)){
-			//System.out.println("added !");
-			//System.out.println(toString());
+		boolean present= false;
+		for (int i = 0; i<this.allowed_actions.size();i++){
+			if (this.allowed_actions.get(i).getClass().getSimpleName().equals(act.getClass().getSimpleName())){
+				present = true;
+			}
 		}
-		notifyObserver();
+		if (!present){
+			this.allowed_actions.add(act);
+			this.notifyObserver();
+		}
 	}
-	
+
 	public void removeActionFromList(int_Action act){
-		allowed_actions.remove(act);
-		notifyObserver();
+		this.allowed_actions.remove(act);
+		this.notifyObserver();
 	}
-	
+
+	@Override
 	public boolean isPresent(int_Action act){
-		return this.allowed_actions.contains(act);
+		boolean present= false;
+		for (int i = 0; i<this.allowed_actions.size();i++){
+			if (this.allowed_actions.get(i).getClass().getSimpleName().equals(act.getClass().getSimpleName())){
+				present = true;
+			}
+		}
+		return present;
 	}
-	
+
 	/*public void execute (Robot r) throws MouvementEx, UnreachableCase, ActionEx{
 		int size = allowed_actions.size();
 		for (int i = 0 ; i < size ; i++ ){
@@ -44,40 +51,41 @@ public class Possible_List implements int_Action_List, int_Observable{
 			act.execute(r);
 		}
 	}*/
-	
+
 	public int_Action pickAction (int index){
-		int_Action ax = allowed_actions.get(index);
-		allowed_actions.remove(index);
-		notifyObserver();
+		int_Action ax = this.allowed_actions.get(index);
+		this.allowed_actions.remove(index);
+		this.notifyObserver();
 		return ax;
 	}
-	
+
+	@Override
 	public String toString(){
-		return allowed_actions.toString();
+		return this.allowed_actions.toString();
 	}
 	public int size(){
-		return allowed_actions.size();
+		return this.allowed_actions.size();
 	}
 	public String get_name(int i){
-		return allowed_actions.get(i).getClass().getSimpleName();
+		return this.allowed_actions.get(i).getClass().getSimpleName();
 	}
-	
+
 	public LinkedList<int_Action> get(){
 		return this.allowed_actions;
 	}
-	
+
 	@Override
 	public void addObserver(int_Observer obs) {
 		this.listObserver.add(obs);
 	}
 	@Override
 	public void removeObserver() {
-		listObserver = new ArrayList<int_Observer>();
-		
+		this.listObserver = new ArrayList<int_Observer>();
+
 	}
 	@Override
 	public void notifyObserver() {
-		for(int_Observer obs : listObserver)
-		      obs.update(this);
+		for(int_Observer obs : this.listObserver)
+			obs.update(this);
 	}
 }
